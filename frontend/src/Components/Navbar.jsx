2887bcd43bd0af66702ad3d71abcef3493188ed6
ajey108 <ajayxa1@gmail.com>
2024-06-13 22:02:31 +0530

    update Navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,35 +1,43 @@
-import React from 'react'
-import logo from  '../assets/logo.png'
-import cart_icon from  '../assets/cart_icon.png'
+import React, { useState } from 'react';
+import logo from '../assets/logo.png';
+import cart_icon from '../assets/cart_icon.png';
 
 const Navbar = () => {
-  return (
-    <div className='flex justify-around p-4 shadow-md '>
+  const [menu, setMenu] = useState("Shop");
 
-      <div className=''>
-      <img src={logo} alt="" className='w-[50px] h-[50px]' />
-      <h1>Shopper</h1>
+  return (
+    <div className='flex justify-around p-4 shadow-md'>
+      <div className='flex items-center'>
+        <img src={logo} alt="Logo" className='w-[40px] h-[40px]' />
+        <h1 className='ml-2 text-xl font-bold'>GadgetStore</h1>
       </div>
       <ul className="flex justify-center items-center gap-4">
-
-      <li>Shop</li>
- <li>Mobile</li>
- <li>Laptop</li>
- <li>Cameras</li>
-
+        <li onClick={() => setMenu('Shop')} className="relative cursor-pointer">
+          Shop
+          {menu === "Shop" && <hr className='absolute bottom-0 left-0 w-full h-[3px] bg-gray-500 rounded-[10px] border-none' />}
+        </li>
+        <li onClick={() => setMenu('Mobile')} className="relative cursor-pointer">
+          Mobile
+          {menu === "Mobile" && <hr className='absolute bottom-0 left-0 w-full h-[3px] bg-gray-500 rounded-[10px] border-none' />}
+        </li>
+        <li onClick={() => setMenu('Laptop')} className="relative cursor-pointer">
+          Laptop
+          {menu === "Laptop" && <hr className='absolute bottom-0 left-0 w-full h-[3px] bg-gray-500 rounded-[10px] border-none' />}
+        </li>
+        <li onClick={() => setMenu('Cameras')} className="relative cursor-pointer">
+          Cameras
+          {menu === "Cameras" && <hr className='absolute bottom-0 left-0 w-full h-[3px] bg-gray-500 rounded-[10px] border-none' />}
+        </li>
       </ul>
-
-      <div className='nav-logcart flex gap-6'>
-        <button className='py-2 px-6 bg-gray-500 rounded-md'>Login</button>
-        <img src={cart_icon} alt="" />
+      <div className="nav-logcart flex gap-6 justify-center items-center relative">
+        <button className="w-[120px] h-[40px] bg-gray-500 text-white rounded-md font-semibold hover:bg-gray-600 hover:scale-105 active:bg-gray-700 active:scale-95 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 transition transform duration-200 ease-in-out">Login</button>
+        <div className="relative">
+          <img src={cart_icon} alt="Cart Icon" className='w-9 h-9' />
+          <div className="absolute top-0 right-0 w-[22px] h-[12px] flex justify-center items-center rounded-full text-[14px] bg-red-500 text-white">0</div>
+        </div>
       </div>
-     
-
-
-
     </div>
- 
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
