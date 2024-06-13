50a4996240f6605c7eaaf774f4d01792488c458d
ajey108 <ajayxa1@gmail.com>
2024-06-13 10:59:50 +0530

    update logo

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import logo from  '../assets/logo.png'
+import cart_icon from  '../assets/cart_icon.png'
 
 const Navbar = () => {
   return (
     <div>
-      <h1>Hi</h1>
- <img src={logo.png} alt="" />
+
+      <div className=''>
+      <img src={logo} alt="" className='w-[50px] h-[50px]' />
+      <h1>Shopper</h1>
+      </div>
+      <ul className=""></ul>
+     
+ <li>Shop</li>
+ <li>Mobile</li>
+ <li>Laptop</li>
+ <li>Cameras</li>
+
 
     </div>
  
