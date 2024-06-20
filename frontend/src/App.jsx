e1668b50336b5fb4fd92eeda8a1b2509bd0b25da
ajey108 <ajayxa1@gmail.com>
2024-06-20 17:22:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+
 
 const App = () => {
   return (
    <>
    
-  <Navbar/>
+   <BrowserRouter>
+   <Navbar/>
+   <Routes>
+    <Route path = '/' element={<Shop/>}/>
+    <Route path = '/mobiles' element={<ShopCategory category="mobile"/>}/>
+    <Route path = '/laptops' element={<ShopCategory category="laptops"/>}/>
+    <Route path = '/cameras' element={<ShopCategory category="cameras"/>}/>
+   <Route path='/product' element={<Product/>}>
+    <Route path=':productId' element={<Product/>}/>
+   </Route>
+   <Route path = '/cart' element={<Cart/>}/>
+   <Route path = '/login' element={<LoginSignup/>}/>
+   </Routes>
+   </BrowserRouter>
+ 
 
    
    </>
