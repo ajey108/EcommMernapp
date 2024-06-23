1f44b2e4fc5abba6a066483dd139481b48b1482c
ajey108 <ajayxa1@gmail.com>
2024-06-23 13:53:15 +0530

    Add item.jsx

---
+++ frontend/src/Components/Items/Item.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Item = (props) => {
+  return (
+    
+    <div className='item w-85'>
+        <img src={props.image}/>
+        <p className='py-2 px-0'>{props.name}</p>
+        <div className="item-prices flex gap-5 ">{/* items price */} 
+
+            <div className="itempricenew color-gray font-semibold text-lg "> {/* items price new */}
+                {props.new_price}
+            </div>
+            <div className="itempriold color-gray font-semibold text-lg decoration-dashed ">{/* items price old*/}
+                {props.old_price}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Item
