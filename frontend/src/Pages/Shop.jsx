20298be0fa7c2f04451d7b4d5285b48e9b9db852
ajey108 <ajayxa1@gmail.com>
2024-06-24 16:21:07 +0530

    Update.Shop.jsx

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers'
+import NewCollections from '../Components/NewCollections'
+import Newsletter from '../Components/NewsLetter'
+import Footer from '../Components/Footer'
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <Offers/>
+      <NewCollections/>
+      <Newsletter/>
+      <Footer/>
     </div>
   )
 }
