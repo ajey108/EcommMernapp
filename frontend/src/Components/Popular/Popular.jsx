2d1ede080f60127eb04ac2d5d2406639dd754c47
ajey108 <ajayxa1@gmail.com>
2024-06-25 20:10:02 +0530

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -4,16 +4,21 @@ import Item from '../Items/Item'
 
 const Popular = () => {
   return (
-  
-   <div className="flex flex-col items-center gap-5 h-[90vh] ">    {/* main div popular  */}
-    <h1 className='font-bold text-4xl'>POPULAR IN MOBILES </h1>
-    <hr className='w-[200px] h-[6px] bg-gray-500 border-sm ' />
-    <div className="mt-8 flex gap-9">
-        {data_product.map((item,i)=>{
-            return <Item key={i} id = {item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+
+    <div className="flex flex-col items-center gap-5 h-[90vh] ">    {/* main div popular  */}
+      <h1 className='font-bold text-4xl'>POPULAR IN MOBILES </h1>
+      <hr className='w-[200px] h-[6px] bg-gray-500 border-sm ' />
+      <div className="mt-8 flex gap-9">
+        {data_product.map((item, i) => {
+          return <Item key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price} />
         })}
+      </div>
     </div>
-   </div>
   )
 }
 
