7c99e61ca5e7782905dbfa3b96715159b22481d9
ajey108 <ajayxa1@gmail.com>
2024-06-25 20:09:45 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,10 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Newsletter from './Components/NewsLetter';
+import Footer from './Components/Footer';
+import Mobileban from './assets/Mobileban.png';
+import lap from './assets/lap.png';
+import camdeal from './assets/camdeal.png';
 
 
 const App = () => {
@@ -18,15 +22,17 @@ const App = () => {
 
    <Routes>
     <Route path = '/' element={<Shop/>}/>
-    <Route path = '/mobiles' element={<ShopCategory category="mobile"/>}/>
-    <Route path = '/laptops' element={<ShopCategory category="laptops"/>}/>
-    <Route path = '/cameras' element={<ShopCategory category="cameras"/>}/>
+    <Route path = '/mobiles' element={<ShopCategory banner={Mobileban} category="mobile"/>}/>
+    <Route path = '/laptops' element={<ShopCategory banner={lap}   category="laptops"/>}/>
+    <Route path = '/cameras' element={<ShopCategory banner={camdeal}  category="cameras"/>}/>
    <Route path='/product' element={<Product/>}>
     <Route path=':productId' element={<Product/>}/>
    </Route>
    <Route path = '/cart' element={<Cart/>}/>
    <Route path = '/login' element={<LoginSignup/>}/>
+
    </Routes>
+   <Footer/>
    </BrowserRouter>
  
 
