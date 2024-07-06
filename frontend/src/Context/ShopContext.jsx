5deb0abba95f28e70db2f91cf8782ebd5d6a56e2
ajey108 <ajayxa1@gmail.com>
2024-07-06 17:02:40 +0530

    Update ShopContext

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,11 +1,35 @@
-import React,{createContext} from "react";
+import React,{createContext, useState} from "react";
 import all_product from '../assets/all_product';
 
 export const ShopContext = createContext(null);
+const getDefaultCart=()=>{
+
+    let cart ={};
+for (let index=0;index< all_product.length+1;index++){
+    cart[index]=0;
+}
+return cart;
+
+}
 
 const ShopContextProvider =(props)=>{
-    const contextValue = {all_product};
 
+
+    const [cartitems,setCartItems] =useState(getDefaultCart());
+  
+
+    //add cart
+    const addToCart =(itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        console.log(cartitems);
+    }
+
+
+    //remove cart
+     const removeCart =(itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+    const contextValue = {all_product,cartitems,addToCart,removeCart};
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
