4de8242a5d8adbaa5cf47bbf8508e45c67cf2dd8
ajey108 <ajayxa1@gmail.com>
2024-07-11 22:20:20 +0530

    Update Index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,91 @@ mongoose.connect("mongodb+srv://ajey3046:H94tqHusPeJ3hobg@cluster0.nxwbegb.mongo
 //Api
 
 app.get('/',(req,res)=>{
-    res.send('The Database is connected')
+    res.send('App is running')
+})
+
+// Multer setup
+
+const storage = multer.diskStorage({
+    destination:'./Uploads/images',
+    filename:((req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    })
+})
+
+const upload = multer({storage:storage})
+
+//Api for Uploading Images
+
+app.use('/images',express.static('Uploads/images'))
+
+app.post('/upload',upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+
+})
+
+// Schema for products
+
+const Products = mongoose.model("Product",{
+    id:{
+        type:Number,
+        require:true,
+    },
+    name:{
+        type:String,
+        require:true,
+    },
+    image:{
+        type:String,
+        require:true,
+    },
+    category:{
+        type:String,
+        require:true,
+    },
+    new_price:{
+        type:Number,
+        require:true,
+    },
+    old_price:{
+        type:Number,
+        require:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+
+    avilable:{
+        type:Boolean,
+        default:true,
+    }
+})
+
+
+//api for add product
+
+app.post('/addproduct',async(req,res)=>{
+    const product = new Products({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price
+    });
+    console.log(product);
+
+    //save product in db
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
 })
 
 app.listen(port,(error)=>{
