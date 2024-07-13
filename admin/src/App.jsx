5a5f1b728b84efdd71f59580bd3168c10a911123
ajey108 <ajayxa1@gmail.com>
2024-07-13 22:16:35 +0530

    setup admin frontend

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Admin from './Pages/Admin'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <>
+   
+   <Navbar/>
+   <Admin/>
+    </>
+  )
+}
+
+export default App
