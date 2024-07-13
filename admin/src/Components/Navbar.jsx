3aaa8c868bef5eb1ac6d828b5c07ca447dab381b
ajey108 <ajayxa1@gmail.com>
2024-07-13 22:16:47 +0530

    Add Navbar.jsx

---
+++ admin/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <div className="container mx-auto flex justify-between items-center border border-4">
+   <h1 className='text-xl md:text-2xl font-bold'>AdminDashboard</h1>
+   
+   </div>
+   
+  )
+}
+
+export default Navbar
