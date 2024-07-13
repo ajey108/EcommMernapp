62bb514a772d5cce15ce76200ced77f941a7c381
ajey108 <ajayxa1@gmail.com>
2024-07-13 23:06:35 +0530

    Update Navbar,Sidebar,Admin.jsx

--- admin/src/Components/Sidebar.jsx
+++ admin/src/Components/Sidebar.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { IoMdAddCircle } from "react-icons/io";
 import { MdViewList } from "react-icons/md";
 
-
 const Sidebar = () => {
     return (
-        <div className="">
-            <Link to={'/addproduct'}>
-                <div className="">
-                    <IoMdAddCircle />
-                    <p>Add Product</p>
-                </div>
+        <div className="w-full sm:w-64 bg-gray-800 text-white h-full flex flex-col p-4">
+            <Link to={'/addproduct'} className="flex items-center space-x-2 p-3 hover:bg-gray-700 rounded-lg transition-colors duration-200 ease-in-out">
+                <IoMdAddCircle className="text-2xl" />
+                <p className="text-lg">Add Product</p>
             </Link>
 
-            <Link to={'/listproduct'}>
-                <div className="">
-                    <MdViewList />
-                    <p>ProductList</p>
-                </div>
+            <Link to={'/listproduct'} className="flex items-center space-x-2 p-3 hover:bg-gray-700 rounded-lg transition-colors duration-200 ease-in-out">
+                <MdViewList className="text-2xl" />
+                <p className="text-lg">Product List</p>
             </Link>
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
--- admin/src/Pages/Admin.jsx
+++ admin/src/Pages/Admin.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Sidebar from '../Components/Sidebar'
+import {Routes,Route} from 'react-router-dom'
+import AddProduct from '../Components/AddProduct'
+import ListProduct from '../Components/ListProduct'
 
 const Admin = () => {
   return (
-<div className="">
+<div className="flex h-screen">
   <Sidebar/>
+  <Routes>
+    <Route path='/addproduct' element={<AddProduct/>}/>
+    <Route path='/addproduct 'element={<ListProduct/>}/>
+  </Routes>
+
 </div>
   )
 }
