07a6e829ec525e0c3c214f7a4531cfe08d6863e1
ajey108 <ajayxa1@gmail.com>
2024-07-14 19:43:28 +0530

    Add app.js renamed index.js to app.js

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const productRoutes = require("./routes/productRoutes");
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+app.use('/images', express.static('uploads/images'));
+
+// Database connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('App is running');
+});
+
+// Routes
+app.use('/api/products', productRoutes);
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log(`Server running on port ${port}`);
+  } else {
+    console.log(`Error: ${error}`);
+  }
+});
