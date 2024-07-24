dcc83d58203dc92f6dfd1c8688bdcff17296ed16
ajey108 <ajayxa1@gmail.com>
2024-07-24 14:49:56 +0530

    Update Navbar.jsx

--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-   <div className=" flex justify-between bg-gray-700 text-white items-center border border-4">
+   <div className=" flex justify-between bg-gray-700 text-white items-center  border-4">
    <h1 className='text-xl md:text-2xl font-bold'>AdminDashboard</h1>
    
    </div>
