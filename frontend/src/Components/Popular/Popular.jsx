1bb45835642514d6f53576e1408658bd06348bbc
ajey108 <ajayxa1@gmail.com>
2024-08-02 15:35:19 +0530

    Update Popular.jsx add fetchmethod

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import data_product from '../../assets/data'
+import React, { useEffect, useState } from 'react'
 import Item from '../Items/Item'
 
 const Popular = () => {
+
+  const [popularProducts,setPopularProducts] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/api/products/popularinmobiles').
+    then((response)=>response.json()).
+    then((data)=>setPopularProducts(data));
+  },[])
   return (
 
     <div className="flex flex-col items-center mt-5 gap-5  ">    {/* main div popular  */}
       <h1 className='font-bold text-4xl'>POPULAR IN MOBILES </h1>
       <hr className='w-[250px] h-[6px] bg-gray-500 border-sm ' />
       <div className="mt-4 flex gap-9">
-        {data_product.map((item, i) => {
+        {popularProducts.map((item, i) => {
           return <Item key={i}
             id={item.id}
             name={item.name}
