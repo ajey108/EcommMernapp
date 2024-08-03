ae0285a1ac72abe97815c2bcc315ae7cef53f244
ajey108 <ajayxa1@gmail.com>
2024-08-03 15:20:35 +0530

    Update authmiddleware.js

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,16 +1,18 @@
 // Fetch user middleware
-export const fetchUser = async (req, res, next) => {
-    const token = req.header('auth');
-    if (!token) {
-      return res.status(401).send({ error: "Please authenticate using a valid token" });
-    }
-    try {
-      const data = jwt.verify(token, process.env.JWT_SECRET || 'secret_ecom');
-      req.user = data.user;
-      next();
-    } catch (err) {
-      return res.status(401).json({ error: err.message });
-    }
-  }
+import jwt from 'jsonwebtoken';
 
-  export default fetchUser;
+export const fetchUser = async (req, res, next) => {
+  const token = req.header('auth-token');
+  if (!token) {
+    return res.status(401).json({ error: "Please authenticate using a valid token" });
+  }
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET || 'secret_ecom');
+    req.user = data.user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
+
+export default fetchUser;
