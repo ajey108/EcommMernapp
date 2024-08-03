0bad8d3b6d896e2e6fd5d7a51619db9a73647658
ajey108 <ajayxa1@gmail.com>
2024-08-03 14:28:14 +0530

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import multer from 'multer';
 import path from 'path';
-import { getAllProducts, addProduct, removeProduct,newCollection, popularinMobiles } from '../controllers/productController.js';
 import { fileURLToPath } from 'url';
+import fetchUser from '../middleware/authmiddleware.js'
+import { getAllProducts, addProduct, removeProduct, newCollection, popularinMobiles, addtoCart } from '../controllers/productController.js';
 
 // Create a __dirname variable to use with ES6 modules
 const __filename = fileURLToPath(import.meta.url);
@@ -25,19 +26,23 @@ router.get('/allproducts', getAllProducts);
 router.post('/addproduct', addProduct);
 router.post('/removeproduct', removeProduct);
 router.get('/newcollection', newCollection);
-router.get('/popularinmobiles', popularinMobiles);//popular in mobiles
-
-
+router.get('/popularinmobiles', popularinMobiles);
+router.post('/addtocart', fetchUser, addtoCart);
 
 
 // API for uploading images
 router.post('/upload', upload.single('product'), (req, res) => {
-    res.json({
-        success: 1,
-        image_url: `http://localhost:${process.env.PORT || 4000}/images/${req.file.filename}`,
-    });
+    if (req.file) {
+        res.json({
+            success: 1,
+            image_url: `http://localhost:${process.env.PORT || 4000}/images/${req.file.filename}`,
+        });
+    } else {
+        res.status(400).json({
+            success: 0,
+            message: 'File upload failed'
+        });
+    }
 });
 
 export default router;
-
-
