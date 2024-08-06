8fa9b212c3898d69a75212383772a7d846376c3e
ajey108 <ajayxa1@gmail.com>
2024-08-06 21:11:11 +0530

    Update authmiddleware.js

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -3,10 +3,16 @@ import jwt from 'jsonwebtoken';
 
 export const fetchUser = async (req, res, next) => {
   const token = req.header('auth-token');
+  console.log(token);
   if (!token) {
+   
     return res.status(401).json({ error: "Please authenticate using a valid token" });
   }
   try {
+
+    const secret = process.env.JWT_SECRET || 'secret_ecom'; 
+    console.log('Secret used for verification:', secret);
+    
     const data = jwt.verify(token, process.env.JWT_SECRET || 'secret_ecom');
     req.user = data.user;
     next();
